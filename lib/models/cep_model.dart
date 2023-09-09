015ae64b8ab0c3c446583eff949012d86abda9aa
jusylopes <jusylopes@outlook.com>
2023-09-08 22:07:22 -0300

    add: cep model

---
+++ lib/models/cep_model.dart
@@ -0,0 +1,50 @@
+class CepModel {
+  final String cep;
+  final String logradouro;
+  final String complemento;
+  final String bairro;
+  final String localidade;
+  final String uf;
+  final String ibge;
+  final String gia;
+  final String ddd;
+  final String siafi;
+  CepModel({
+    required this.cep,
+    required this.logradouro,
+    required this.complemento,
+    required this.bairro,
+    required this.localidade,
+    required this.uf,
+    required this.ibge,
+    required this.gia,
+    required this.ddd,
+    required this.siafi,
+  });
+
+  CepModel copyWith({
+    String? cep,
+    String? logradouro,
+    String? complemento,
+    String? bairro,
+    String? localidade,
+    String? uf,
+    String? ibge,
+    String? gia,
+    String? ddd,
+    String? siafi,
+  }) {
+    return CepModel(
+      cep: cep ?? this.cep,
+      logradouro: logradouro ?? this.logradouro,
+      complemento: complemento ?? this.complemento,
+      bairro: bairro ?? this.bairro,
+      localidade: localidade ?? this.localidade,
+      uf: uf ?? this.uf,
+      ibge: ibge ?? this.ibge,
+      gia: gia ?? this.gia,
+      ddd: ddd ?? this.ddd,
+      siafi: siafi ?? this.siafi,
+    );
+  }
+}
