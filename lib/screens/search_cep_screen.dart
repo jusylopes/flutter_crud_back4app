fef6ea4f155a5db60da1eacb8b17d5047a7733e6
jusylopes <jusylopes@outlook.com>
2023-09-30 01:55:02 -0300

    refactor: add form field

--- lib/screens/search_cep_screen.dart
+++ lib/screens/search_cep_screen.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud_back4app/blocs/cep/enum/bloc_status.dart';
-import 'package:flutter_crud_back4app/blocs/cep/search/search_cep_bloc.dart';
 import 'package:flutter_crud_back4app/blocs/cep/search/search_cep_blocs_exports.dart';
-import 'package:flutter_crud_back4app/components/cep_form_field.dart';
 import 'package:flutter_crud_back4app/components/cep_information_card.dart';
 import 'package:flutter_crud_back4app/components/error_message_app.dart';
-import 'package:flutter_crud_back4app/models/cep_model.dart';
 import 'package:flutter_crud_back4app/screens/register_cep_screen.dart';
 import 'package:flutter_crud_back4app/utils/assets_manager.dart';
 import 'package:flutter_crud_back4app/utils/colors.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class SearchCepScreen extends StatefulWidget {
   const SearchCepScreen({super.key});
@@ -18,6 +16,29 @@ class SearchCepScreen extends StatefulWidget {
 }
 
 class _SearchCepScreenState extends State<SearchCepScreen> {
+  late final GlobalKey<FormState> _formKey;
+  late final TextEditingController _cepController;
+
+  @override
+  void initState() {
+    super.initState();
+    _formKey = GlobalKey<FormState>();
+    _cepController = TextEditingController();
+  }
+
+  _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+    }
+  }
+
+  _submitCep(String? value) {
+    if (_formKey.currentState?.validate() ?? false) {
+      FocusScope.of(context).unfocus();
+      BlocProvider.of<SearchCepBloc>(context).add(GetCep(cep: value ?? ''));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -30,12 +51,60 @@ class _SearchCepScreenState extends State<SearchCepScreen> {
               children: [
                 Padding(
                   padding: const EdgeInsets.only(top: 10),
-                  child: Image.asset(AssetsManager.imageLogo),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(AssetsManager.imageLogo),
+                      const SizedBox(width: 10),
+                      Text(
+                        'Busca CEP',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                    ],
+                  ),
                 ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 20),
-                  child: CepFormField(),
-                )
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 20),
+                  child: Form(
+                    key: _formKey,
+                    child: TextFormField(
+                      autofocus: false,
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.done,
+                      controller: _cepController,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          hintText: 'Informe um cep*',
+                          suffixIcon: IconButton(
+                            iconSize: 35,
+                            onPressed: _submitForm,
+                            icon: const Icon(Icons.search),
+                          ),
+                          hintStyle: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w400)),
+                      inputFormatters: [
+                        MaskTextInputFormatter(
+                          mask: '#####-###',
+                          filter: {"#": RegExp(r'[0-9]')},
+                        ),
+                      ],
+                      onSaved: (value) => _submitCep(value),
+                      onFieldSubmitted: (value) => _submitCep(value),
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            value.length != 9) {
+                          return 'Formato de CEP inválido';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
               ],
             ),
             bottom: PreferredSize(
@@ -57,7 +126,7 @@ class _SearchCepScreenState extends State<SearchCepScreen> {
                     return const CircularProgressIndicator();
                   case BlocStatus.success:
                     if (state.isEmpty) {
-                      return (const RegisterAddress());
+                      return AddressEmpty(cepController: _cepController);
                     } else {
                       return CepInformationCard(address: state.address!);
                     }
@@ -71,12 +140,21 @@ class _SearchCepScreenState extends State<SearchCepScreen> {
           )),
     );
   }
+
+  @override
+  void dispose() {
+    _cepController.dispose();
+    super.dispose();
+  }
 }
 
-class RegisterAddress extends StatelessWidget {
-  const RegisterAddress({
+class AddressEmpty extends StatelessWidget {
+  const AddressEmpty({
     super.key,
-  });
+    required TextEditingController cepController,
+  }) : _cepController = cepController;
+
+  final TextEditingController _cepController;
 
   @override
   Widget build(BuildContext context) {
@@ -99,7 +177,9 @@ class RegisterAddress extends StatelessWidget {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                    builder: (context) => const RegisterCepScreen()),
+                    builder: (context) => RegisterCepScreen(
+                          inicialCep: _cepController.text,
+                        )),
               );
             },
           ),
