cb1ac4862afca5b1c2f7f14b347c3598280071f9
jusylopes <jusylopes@outlook.com>
2023-10-03 00:57:24 -0300

    add: check cep back4app

--- lib/repositories/cep_repository.dart
+++ lib/repositories/cep_repository.dart
@@ -4,16 +4,12 @@ import 'package:flutter_crud_back4app/models/cep_model.dart';
 class CepRepository {
   final Dio dio;
 
-  CepRepository({required this.dio}) {
-    dio.options.headers['X-Parse-Application-Id'] = '';
-    dio.options.headers['X-Parse-REST-API-Key'] = '';
-    dio.options.headers['Content-Type'] = 'application/json';
-  }
+  CepRepository({required this.dio});
 
   static const String _baseUrlBack4app =
-      "https://parseapi.back4app.com/classes/ceps";
+      'https://parseapi.back4app.com/classes/ceps';
 
-  Future<CepModel> fetchCep({required String cep}) async {
+  Future<CepModel> fetchCepViaCep({required String cep}) async {
     try {
       Response response = await dio.get('https://viacep.com.br/ws/$cep/json');
       final data = response.data;
@@ -28,12 +24,13 @@ class CepRepository {
     }
   }
 
-  Future<void> addCep(CepModel newCep) async {
+  Future<bool> checkCepBack4app({required cepToFind}) async {
     try {
-      await dio.post(
-        _baseUrlBack4app,
-        data: newCep.toJson(),
-      );
+      Response response =
+          await dio.get('$_baseUrlBack4app?where={"cep":"$cepToFind"}');
+      final data = response.data['results'] as List<dynamic>;
+
+      return data.isNotEmpty;
     } catch (e) {
       rethrow;
     }
@@ -49,6 +46,17 @@ class CepRepository {
           .toList();
 
       return ceps;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> addCep(CepModel newCep) async {
+    try {
+      await dio.post(
+        _baseUrlBack4app,
+        data: newCep.toJson(),
+      );
     } catch (e) {
       rethrow;
     }
