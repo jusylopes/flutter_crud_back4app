2336c2620bb1dbcd73d3cfbfb0b60762ecf2ed71
jusylopes <jusylopes@outlook.com>
2023-10-11 11:02:18 -0300

    refactor: cep input formatter

--- lib/screens/search_cep_screen.dart
+++ lib/screens/search_cep_screen.dart
@@ -8,7 +8,7 @@ import 'package:flutter_crud_back4app/models/cep_model.dart';
 import 'package:flutter_crud_back4app/screens/register_cep_screen.dart';
 import 'package:flutter_crud_back4app/utils/assets_manager.dart';
 import 'package:flutter_crud_back4app/utils/colors.dart';
-import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:flutter_crud_back4app/utils/cep_input_formatter.dart';
 
 class SearchCepScreen extends StatefulWidget {
   const SearchCepScreen({super.key});
@@ -48,109 +48,103 @@ class _SearchCepScreenState extends State<SearchCepScreen> {
         FocusScope.of(context).requestFocus(FocusNode());
       },
       child: Scaffold(
-          appBar: AppBar(
-            title: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset(AssetsManager.imageLogo),
-                      const SizedBox(width: 10),
-                      Text(
-                        'Busca CEP',
-                        style: Theme.of(context).textTheme.titleLarge,
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 20),
-                  child: Form(
-                    key: _formKey,
-                    child: TextFormField(
-                      autofocus: false,
-                      keyboardType: TextInputType.number,
-                      textInputAction: TextInputAction.done,
-                      controller: _cepController,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                          hintText: 'Informe um cep*',
-                          suffixIcon: IconButton(
-                            iconSize: 35,
-                            onPressed: _submitForm,
-                            icon: const Icon(Icons.search),
-                          ),
-                          hintStyle: const TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.w400)),
-                      inputFormatters: [
-                        MaskTextInputFormatter(
-                          mask: '#####-###',
-                          filter: {"#": RegExp(r'[0-9]')},
-                        ),
-                      ],
-                      onSaved: (value) => _submitCep(value),
-                      onFieldSubmitted: (value) => _submitCep(value),
-                      validator: (value) {
-                        if (value == null ||
-                            value.isEmpty ||
-                            value.length != 9) {
-                          return 'Formato de CEP inválido';
-                        }
-                        return null;
-                      },
+        appBar: AppBar(
+          title: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(AssetsManager.imageLogo),
+                    const SizedBox(width: 10),
+                    Text(
+                      'Busca CEP',
+                      style: Theme.of(context).textTheme.titleLarge,
                     ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 20),
+                child: Form(
+                  key: _formKey,
+                  child: TextFormField(
+                    autofocus: false,
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.done,
+                    controller: _cepController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        hintText: 'Informe um cep*',
+                        suffixIcon: IconButton(
+                          iconSize: 35,
+                          onPressed: _submitForm,
+                          icon: const Icon(Icons.search),
+                        ),
+                        hintStyle: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w400)),
+                    inputFormatters: [CepInputFormatter()],
+                    onSaved: (value) => _submitCep(value),
+                    onFieldSubmitted: (value) => _submitCep(value),
+                    validator: (value) {
+                      if (value == null || value.isEmpty || value.length != 9) {
+                        return 'Formato de CEP inválido';
+                      }
+                      return null;
+                    },
                   ),
                 ),
-              ],
-            ),
-            automaticallyImplyLeading: false,
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(2),
-              child: Container(
-                color: AppColors.secondaryColor,
-                height: 2,
               ),
-            ),
-            toolbarHeight: MediaQuery.of(context).size.height / 4,
+            ],
           ),
-          body: Center(
-            child: BlocBuilder<SearchCepBloc, SearchCepState>(
-              builder: (context, state) {
-                switch (state.status) {
-                  case BlocStatus.initial:
-                    return const Center();
-                  case BlocStatus.loading:
-                    return const CircularProgressIndicator();
-                  case BlocStatus.success:
-                    final CepModel address = state.address!;
-
-                    if (address.bairro.isEmpty) {
-                      return const AddressEmpty();
-                    } else {
-                      return Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          CepInformationCard(address: address),
-                          if (state.isRegisteredCep == false)
-                            UnregisteredAddress(address: address),
-                        ],
-                      );
-                    }
-
-                  case BlocStatus.error:
-                    return ErrorMessageViaCep(
-                      errorMessage: state.errorMessage!,
-                    );
-                }
-              },
+          automaticallyImplyLeading: false,
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(2),
+            child: Container(
+              color: AppColors.secondaryColor,
+              height: 2,
             ),
-          )),
+          ),
+          toolbarHeight: MediaQuery.of(context).size.height / 4,
+        ),
+        body: Center(
+          child: BlocBuilder<SearchCepBloc, SearchCepState>(
+            builder: (context, state) {
+              switch (state.status) {
+                case BlocStatus.initial:
+                  return const Center();
+                case BlocStatus.loading:
+                  return const CircularProgressIndicator();
+                case BlocStatus.success:
+                  final CepModel address = state.address!;
+
+                  if (address.bairro.isEmpty) {
+                    return const AddressEmpty();
+                  } else {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CepInformationCard(address: address),
+                        if (state.isRegisteredCep == false)
+                          UnregisteredAddress(address: address),
+                      ],
+                    );
+                  }
+
+                case BlocStatus.error:
+                  return ErrorMessageApp(
+                    errorMessage: state.errorMessage!,
+                  );
+              }
+            },
+          ),
+        ),
+      ),
     );
   }
 
